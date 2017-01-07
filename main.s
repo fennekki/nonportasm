6bd325df4bbb3b5acd5b48cc3003dba785b9f6bb
fennekki <fennekki@iki.fi>
2017-01-07 04:54:18 +0200

    Add safeguard for allocation error

--- main.s
+++ main.s
@@ -5,6 +5,9 @@ startup_msg_len: .quad (. - startup_msg) # this point - startup_msg
 test_str: .string "yiff\n"
 test_str_len: .quad (. - test_str)
 
+alloc_error_msg: .string "Failed to allocate memory\n"
+alloc_error_msg_len: .quad (. - alloc_error_msg)
+
 .text
 .global _start # entry point
 _start:
@@ -16,6 +19,9 @@ _start:
     movq (test_str_len), %rdi # Allocate a bit of memory
     callq easy_alloc
     movq %rax, %r12 # Save address in r12
+    
+    testq %rax, %rax # See if we succeeded in allocating
+    jz alloc_error # If zero, quit with error
 
     # Copy in data
     cld # Clear direction (reverse) flag so we move forward
@@ -35,3 +41,12 @@ _start:
 
     xorq %rdi, %rdi # No error, return 0
     jmp exit # callq sys_exit
+
+alloc_error:
+    movq $2, %rdi # stderr
+    movq $alloc_error_msg, %rsi
+    movq (alloc_error_msg_len), %rdx
+    callq write
+    
+    movq $1, %rdi # Return 1
+    jmp exit
